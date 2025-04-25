cd034068e63bca7bf9ad408fc09bce6000d4698c
ovie <usmanovie128@gmail.com>
2025-04-25 19:13:29 +0100

    done

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const uploadRoutes = require("./routes/uploadRoutes")
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://task-manager-frontend0.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
